83b2ea106fe9c2413bff4a921c916d0d9b18c460
Enrique mota <enrique.motadejesus@gmail.com>
2023-11-28 15:10:44 -0400

    Playing computer

--- main.py
+++ main.py
@@ -9,20 +9,22 @@
 # my_function()
 
 # ? Reproduce the Bug
-from random import randint
+# from random import randint
 
-dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-#! randint is inclusive of the upper bound, so when the number is six on a list with length 5 it will throw an error
-#! to fix it, let's change to randint(0, 5)
-dice_num = randint(0, 5)
-print(dice_imgs[dice_num])
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# #! randint is inclusive of the upper bound, so when the number is six on a list with length 5 it will throw an error
+# #! to fix it, let's change to randint(0, 5)
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
 
 # ? Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+    print("You are a millenial.")
+  #! The code skipped 1994 because it didn't include it in the condition 
+  #! to fix it, let's change to >= instead of > 1994. 
+elif year >= 1994:
+    print("You are a Gen Z.")
 
 # ? Fix the Errors
 # age = input("How old are you?")
