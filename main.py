dea30d7dbb0a911e025203b1d312c22d64c51113
Enrique mota <enrique.motadejesus@gmail.com>
2023-11-28 15:05:54 -0400

    Reproducing the error

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 ################### DEBUGGING #####################
 
-#? Describe Problem
-def my_function():
-    #! i will never reach 20 because range is not inclusive of the upper bound.
-  for i in range(1, 20):
-    if i == 19:
-      print("You got it")
-my_function()
+# ? Describe Problem
+# def my_function():
+#     #! i will never reach 20 because range is not inclusive of the upper bound.
+#   for i in range(1, 20):
+#     if i == 19:
+#       print("You got it")
+# my_function()
 
-#? Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+# ? Reproduce the Bug
+from random import randint
 
-#? Play Computer
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+#! randint is inclusive of the upper bound, so when the number is six on a list with length 5 it will throw an error
+#! to fix it, let's change to randint(0, 5)
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
+
+# ? Play Computer
 # year = int(input("What's your year of birth?"))
 # if year > 1980 and year < 1994:
 #   print("You are a millenial.")
 # elif year > 1994:
 #   print("You are a Gen Z.")
 
-#? Fix the Errors
+# ? Fix the Errors
 # age = input("How old are you?")
 # if age > 18:
 # print("You can drive at age {age}.")
 
-#? Print is Your Friend
+# ? Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
@@ -34,7 +37,7 @@ my_function()
 # total_words = pages * word_per_page
 # print(total_words)
 
-#? Use a Debugger
+# ? Use a Debugger
 # def mutate(a_list):
 #   b_list = []
 #   for item in a_list:
